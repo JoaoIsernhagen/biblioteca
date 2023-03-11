867f570cb5ea33c4bef0c17c3761247541548f91
Joao Isernhage <joaoricardo318@hotmail.com>
2023-03-10 21:20:59 -0300

    .

--- src/Emprestimos.java
+++ src/Emprestimos.java
@@ -1,9 +1,55 @@
-public class Emprestimos {
-    private Livros dataDoEmprestimo;
-    private Usuarios horaDoEmprestimos;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
-    
+public class Emprestimos {
+    private LocalDate dataDoEmprestimo;
+    private LocalTime horaDoEmprestimos;
+    private Livros livro;
+    private Usuarios usuario;
+
+    public Emprestimos(Livros livro, Usuarios usuario) {
+        this.dataDoEmprestimo = LocalDate.now();
+        this.horaDoEmprestimos = LocalTime.now();
+        this.livro = livro;
+        this.usuario = usuario;
+        this.livro.setEmprestimo(true);
+    }
+
+
+    public LocalDate getDataDoEmprestimo() {
+        return dataDoEmprestimo;
+    }
+
+    public void setDataDoEmprestimo(LocalDate dataDoEmprestimo) {
+        this.dataDoEmprestimo = dataDoEmprestimo;
+    }
+
+    public LocalTime getHoraDoEmprestimos() {
+        return horaDoEmprestimos;
+    }
+
+    public void setHoraDoEmprestimos(LocalTime horaDoEmprestimos) {
+        this.horaDoEmprestimos = horaDoEmprestimos;
+    }
+
+    public Livros getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livros livro) {
+        this.livro = livro;
+    }
+
+    public Usuarios getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuarios usuario) {
+        this.usuario = usuario;
+    }
+
     public void devolverLivro(){
+        livro.setEmprestimo(false);
         System.out.println("LIVRO DEVOLVIDO");
     }
 
--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,26 @@ public class Main {
     public static void main(String[] args) {
 
         Livros livros = new Livros();
-        Usuarios usuarios = new Usuarios();
-        Emprestimos emprestimos = new Emprestimos(livros, usuarios);
-
         livros.setTitulo("AMOR");
         livros.setAno("20/10/2012");
 
-
+        Usuarios usuarios = new Usuarios();
         usuarios.setNome("MICHELLE");
 
-
+        Emprestimos emprestimos = new Emprestimos(livros,usuarios);
 
         System.out.println("-----EMPRÉSTIMO-----");
         System.out.println("LIVRO: " + livros.getTitulo());
         System.out.println("NOME: " + usuarios.getNome());
         System.out.println("DATA: " + livros.getAno());
 
+        livros.abrirLivro();
 
+        usuarios.lerLivro();
+
+        livros.fecharLivro();
+
+        emprestimos.devolverLivro();
 
 
     }
